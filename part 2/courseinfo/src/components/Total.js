ebdff07f91c55a798c1069ef155f31dd95c3f8bc
Pikapii77 <91602901+Pikapii77@users.noreply.github.com>
2022-02-24 11:51:55 +0200

    Exercises 2.1 to 2.5

---
+++ part 2/courseinfo/src/components/Total.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((total, part) => {
+          return total + part.exercises
+        }, 0
+  );
+  return(
+    <b>total of {sum} exercises</b>
+  ) 
+}
+
+export default Total;
