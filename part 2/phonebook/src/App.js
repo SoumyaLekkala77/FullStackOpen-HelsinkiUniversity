b95f75c799e2a2ca3fb4150ce0de2001532eda36
Pikapii77 <91602901+Pikapii77@users.noreply.github.com>
2022-02-26 21:27:47 +0200

    Exercise 2.19 - 2.20

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import Notification from "./components/Notification";
 import personService from "./services/PersonService";
 
 const App = () => {
@@ -9,12 +10,20 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
+    const [message, setMessage] = useState(null)
+    const [error, setError] = useState(null)
     useEffect( () =>{
         personService.getAllPersons()
             .then( allEntries => setPersons(allEntries) )
         }, [])
 
     const personsToShow = filter === '' ? persons : persons.filter(person => person.name.toUpperCase().startsWith(filter.toUpperCase()))
+
+    const setNotification = (message, isError) => {
+        if (isError) setError(message)
+        else setMessage(message)
+        setTimeout( () => isError ? setError(null) : setMessage(null), 5000 )
+    }
 
     const addName = (event) => {
         event.preventDefault()
@@ -25,10 +34,21 @@ const App = () => {
             window.confirm(`${duplicates[0].name} is already added to phonebook, replace the old number with a new one?`)
         ){
             personService.updatePerson(duplicates[0].id, data)
-                .then( updatedPerson => setPersons( persons.map( p => p.id === duplicates[0].id ? updatedPerson : p ) ) )
+                .then( updatedPerson => {
+                    if (updatedPerson == null){
+                        setNotification(`Information of ${duplicates[0].name} has already been removed from server`, true)
+                        return
+                    }
+                    console.log("updating person now..")
+                    setPersons( persons.map( p => p.id === duplicates[0].id ? updatedPerson : p ) )
+                    setNotification(`Updated number of ${updatedPerson.name}`, false)
+                })
         }else {
             personService.addPerson( data )
-                .then( newPerson => setPersons( persons.concat(newPerson) ) )
+                .then( newPerson => {
+                    setPersons( persons.concat(newPerson) )
+                    setNotification(`Added ${newPerson.name}`, false)
+                })
         }
 
         setNewName("")
@@ -38,10 +58,17 @@ const App = () => {
     const removePerson = (id) => {
         const person = persons.find( p => p.id === id )
         if ( window.confirm(`Delete ${person.name}?`) )
-        personService.deletePerson(id)
-                .then( response => setPersons( persons.filter(p => p.id !== id)) )
+            personService.deletePerson(id)
+                .then( response => {
+                    if (response == null){
+                        setNotification(`Information of ${person.name} has already been removed from server`, true)
+                        return
+                    }
+                    setPersons( persons.filter(p => p.id !== id))
+                    setNotification(`Removed person ${person.name}`, false)
+                })
     }
-
+    
     const handleNewNameChange = (event) => setNewName(event.target.value)
     const handleNewNumberChange = (event) => setNewNumber(event.target.value)
     const handleFilterChange = (event) => setFilter(event.target.value)
@@ -49,15 +76,17 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={message} isError={false} />
+            <Notification message={error} isError={true} />
             <Filter filter={filter} changeHandler={handleFilterChange}/>
-            <h3>Add a new</h3>
+            <h3>add a new</h3>
             <PersonForm
                 newName={newName}
                 newNameChangeHandler={handleNewNameChange}
                 newNumber={newNumber}
                 newNumberChangeHandler={handleNewNumberChange}
                 addName={addName}/>
-            <h3>Numbers</h3>
+            <h3>numbers</h3>
             <Persons personsToShow={personsToShow} deleteHandler={removePerson}/>
         </div>
     )
--- part 2/phonebook/src/services/PersonService.js
+++ part 2/phonebook/src/services/PersonService.js
@@ -4,21 +4,23 @@ const baseUrl = "http://localhost:3001/persons"
 
 const addPerson = newEntry => {
     return axios.post(baseUrl, newEntry).then( response => response.data )
-}
-
-const updatePerson = (id, person) => {
-    return axios.put(`${baseUrl}/${id}`, person)
-        .then(response => response.data)
+    .catch( error => console.log("Failed to add new person", error) )
 }
 
 const getAllPersons = () => {
     return axios.get(baseUrl).then(response => response.data)
 }
 
+const updatePerson = (id, person) => {
+    return axios.put(`${baseUrl}/${id}`, person)
+        .then(response => response.data)
+        .catch( error => console.log("Failed to update the person") )
+}
+
 const deletePerson = (id) => {
   return axios.delete(`${baseUrl}/${id}`)
       .then( response => response.data )
-      .catch( error => console.log("Failed to delete", error) )
+      .catch( error => console.log("Failed to delete") )
 }
 
 export default {addPerson, updatePerson, getAllPersons, deletePerson}
