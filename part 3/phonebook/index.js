a0cf76bdcd2ce20ad3d411bf0178245f6e642516
Pikapii77 <91602901+Pikapii77@users.noreply.github.com>
2022-02-28 01:03:19 +0200

    Exercise part 3.19

--- part 3/phonebook/index.js
+++ part 3/phonebook/index.js
@@ -42,7 +42,7 @@ app.delete("/api/persons/:id", (request, response, next)=> {
     }).catch( error => next(error) )
 })
 
-app.post("/api/persons", (request, response)=>{
+app.post("/api/persons", (request, response, next)=>{
     const newEntry = request.body
     if (newEntry === undefined) response.status(400).json( {error:"Content missing"} )
     if (!newEntry.name) return response.status(400).json({error:"Missing name property"})
@@ -52,7 +52,9 @@ app.post("/api/persons", (request, response)=>{
         name: newEntry.name,
         number: newEntry.number
     } )
-    person.save().then( savedPerson => response.json(savedPerson) )
+    person.save()
+        .then( savedPerson => response.json(savedPerson) )
+        .catch( error => next(error) )
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -73,6 +75,7 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' })
+    else if (error.name === 'ValidationError') return response.status(400).json({ error: error.message })
     next(error)
 }
 app.use( errorHandler )
