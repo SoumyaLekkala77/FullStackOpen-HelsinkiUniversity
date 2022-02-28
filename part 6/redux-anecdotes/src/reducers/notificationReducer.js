897d292bfca09d052147890ceb4bfa7d9561371c
Pikapii77 <91602901+Pikapii77@users.noreply.github.com>
2022-02-28 20:45:38 +0200

    Exercise part 6.15-6.18

---
+++ part 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,38 @@
+let timeId = 0
+const notificationReducer = (state = null, action) => {
+    switch (action.type) {
+      case 'SHOW_NOTIFICATION': {
+        return action.data.notification
+      }
+      case 'REMOVE_NOTIFICATION':
+          return null
+      default:
+        return state
+    }
+}
+
+export const setNotification = (notification, time) => {
+  return async (dispatch) => {
+    clearTimeout(timeId)
+    timeId = setTimeout(
+      () =>
+        dispatch(removeNotification()),
+      time * 1000
+    )
+
+    dispatch({
+      type: 'SHOW_NOTIFICATION',
+      data: {
+        notification,
+      },
+    })
+  }
+}
+
+export const removeNotification = () => {
+    return {
+        type: 'REMOVE_NOTIFICATION'
+    }
+}
+
+export default notificationReducer
